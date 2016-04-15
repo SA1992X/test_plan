b043ec21b788712fa0a544976e8df7f2dcd7b4cb
SA1992X <825983081@qq.com>
2016-04-15 20:32:06 +0800

    ssa

---
+++ MurlocPaladin/Form1.Designer.cs
@@ -0,0 +1,305 @@
+﻿namespace MurlocPaladin
+{
+    partial class MurlocPaladin
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtOldMurk_Eye = new System.Windows.Forms.TextBox();
+            this.txtWarleader = new System.Windows.Forms.TextBox();
+            this.txtBluegillWarrior = new System.Windows.Forms.TextBox();
+            this.txtGrimscaleOracle = new System.Windows.Forms.TextBox();
+            this.btnThatsAwesome = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtNoFish = new System.Windows.Forms.TextBox();
+            this.txtOldMurk_Eye2 = new System.Windows.Forms.TextBox();
+            this.txtWarleader2 = new System.Windows.Forms.TextBox();
+            this.txtBluegillWarrior2 = new System.Windows.Forms.TextBox();
+            this.txtGrimscaleOracle2 = new System.Windows.Forms.TextBox();
+            this.txtNoFish2 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(31, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "老瞎眼";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("宋体", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(1, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(133, 29);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "鱼人领军";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("宋体", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(1, 136);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(133, 29);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "蓝腮战士";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("宋体", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label4.Location = new System.Drawing.Point(1, 180);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(133, 29);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "暗鳞先知";
+            // 
+            // txtOldMurk_Eye
+            // 
+            this.txtOldMurk_Eye.Location = new System.Drawing.Point(140, 60);
+            this.txtOldMurk_Eye.Name = "txtOldMurk_Eye";
+            this.txtOldMurk_Eye.Size = new System.Drawing.Size(100, 21);
+            this.txtOldMurk_Eye.TabIndex = 1;
+            this.txtOldMurk_Eye.Text = "0";
+            // 
+            // txtWarleader
+            // 
+            this.txtWarleader.Location = new System.Drawing.Point(140, 100);
+            this.txtWarleader.Name = "txtWarleader";
+            this.txtWarleader.Size = new System.Drawing.Size(100, 21);
+            this.txtWarleader.TabIndex = 1;
+            this.txtWarleader.Text = "0";
+            // 
+            // txtBluegillWarrior
+            // 
+            this.txtBluegillWarrior.Location = new System.Drawing.Point(140, 144);
+            this.txtBluegillWarrior.Name = "txtBluegillWarrior";
+            this.txtBluegillWarrior.Size = new System.Drawing.Size(100, 21);
+            this.txtBluegillWarrior.TabIndex = 1;
+            this.txtBluegillWarrior.Text = "0";
+            // 
+            // txtGrimscaleOracle
+            // 
+            this.txtGrimscaleOracle.Location = new System.Drawing.Point(140, 188);
+            this.txtGrimscaleOracle.Name = "txtGrimscaleOracle";
+            this.txtGrimscaleOracle.Size = new System.Drawing.Size(100, 21);
+            this.txtGrimscaleOracle.TabIndex = 1;
+            this.txtGrimscaleOracle.Text = "0";
+            // 
+            // btnThatsAwesome
+            // 
+            this.btnThatsAwesome.Font = new System.Drawing.Font("宋体", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnThatsAwesome.Location = new System.Drawing.Point(456, 165);
+            this.btnThatsAwesome.Name = "btnThatsAwesome";
+            this.btnThatsAwesome.Size = new System.Drawing.Size(232, 50);
+            this.btnThatsAwesome.TabIndex = 2;
+            this.btnThatsAwesome.Text = "计算伤害";
+            this.btnThatsAwesome.UseVisualStyleBackColor = true;
+            this.btnThatsAwesome.Click += new System.EventHandler(this.btnThatsAwesome_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Font = new System.Drawing.Font("宋体", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lblResult.Location = new System.Drawing.Point(439, 52);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(90, 97);
+            this.lblResult.TabIndex = 3;
+            this.lblResult.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("宋体", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label5.Location = new System.Drawing.Point(31, 232);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(103, 29);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "杂牌鱼";
+            // 
+            // txtNoFish
+            // 
+            this.txtNoFish.Location = new System.Drawing.Point(140, 240);
+            this.txtNoFish.Name = "txtNoFish";
+            this.txtNoFish.Size = new System.Drawing.Size(100, 21);
+            this.txtNoFish.TabIndex = 1;
+            this.txtNoFish.Text = "0";
+            // 
+            // txtOldMurk_Eye2
+            // 
+            this.txtOldMurk_Eye2.Location = new System.Drawing.Point(274, 60);
+            this.txtOldMurk_Eye2.Name = "txtOldMurk_Eye2";
+            this.txtOldMurk_Eye2.Size = new System.Drawing.Size(100, 21);
+            this.txtOldMurk_Eye2.TabIndex = 1;
+            this.txtOldMurk_Eye2.Text = "0";
+            // 
+            // txtWarleader2
+            // 
+            this.txtWarleader2.Location = new System.Drawing.Point(274, 100);
+            this.txtWarleader2.Name = "txtWarleader2";
+            this.txtWarleader2.Size = new System.Drawing.Size(100, 21);
+            this.txtWarleader2.TabIndex = 1;
+            this.txtWarleader2.Text = "0";
+            // 
+            // txtBluegillWarrior2
+            // 
+            this.txtBluegillWarrior2.Location = new System.Drawing.Point(274, 144);
+            this.txtBluegillWarrior2.Name = "txtBluegillWarrior2";
+            this.txtBluegillWarrior2.Size = new System.Drawing.Size(100, 21);
+            this.txtBluegillWarrior2.TabIndex = 1;
+            this.txtBluegillWarrior2.Text = "0";
+            // 
+            // txtGrimscaleOracle2
+            // 
+            this.txtGrimscaleOracle2.Location = new System.Drawing.Point(274, 188);
+            this.txtGrimscaleOracle2.Name = "txtGrimscaleOracle2";
+            this.txtGrimscaleOracle2.Size = new System.Drawing.Size(100, 21);
+            this.txtGrimscaleOracle2.TabIndex = 1;
+            this.txtGrimscaleOracle2.Text = "0";
+            // 
+            // txtNoFish2
+            // 
+            this.txtNoFish2.Location = new System.Drawing.Point(274, 240);
+            this.txtNoFish2.Name = "txtNoFish2";
+            this.txtNoFish2.Size = new System.Drawing.Size(100, 21);
+            this.txtNoFish2.TabIndex = 1;
+            this.txtNoFish2.Text = "0";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("宋体", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label6.Location = new System.Drawing.Point(137, 9);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(73, 29);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "墓地";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("宋体", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label7.Location = new System.Drawing.Point(264, 9);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(133, 29);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "对手站场";
+            // 
+            // btnReset
+            // 
+            this.btnReset.Font = new System.Drawing.Font("宋体", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnReset.Location = new System.Drawing.Point(456, 221);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(232, 40);
+            this.btnReset.TabIndex = 4;
+            this.btnReset.Text = "重置";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("方正舒体", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label8.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.label8.Location = new System.Drawing.Point(529, 277);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(159, 17);
+            this.label8.TabIndex = 5;
+            this.label8.Text = "By  1WaYoRaNotHer";
+            // 
+            // MurlocPaladin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(701, 303);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.btnThatsAwesome);
+            this.Controls.Add(this.txtNoFish2);
+            this.Controls.Add(this.txtNoFish);
+            this.Controls.Add(this.txtGrimscaleOracle2);
+            this.Controls.Add(this.txtBluegillWarrior2);
+            this.Controls.Add(this.txtGrimscaleOracle);
+            this.Controls.Add(this.txtWarleader2);
+            this.Controls.Add(this.txtBluegillWarrior);
+            this.Controls.Add(this.txtOldMurk_Eye2);
+            this.Controls.Add(this.txtWarleader);
+            this.Controls.Add(this.txtOldMurk_Eye);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label1);
+            this.Name = "MurlocPaladin";
+            this.Text = "呜哇啦啦啦哇啦";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtOldMurk_Eye;
+        private System.Windows.Forms.TextBox txtWarleader;
+        private System.Windows.Forms.TextBox txtBluegillWarrior;
+        private System.Windows.Forms.TextBox txtGrimscaleOracle;
+        private System.Windows.Forms.Button btnThatsAwesome;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtNoFish;
+        private System.Windows.Forms.TextBox txtOldMurk_Eye2;
+        private System.Windows.Forms.TextBox txtWarleader2;
+        private System.Windows.Forms.TextBox txtBluegillWarrior2;
+        private System.Windows.Forms.TextBox txtGrimscaleOracle2;
+        private System.Windows.Forms.TextBox txtNoFish2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ MurlocPaladin/Form1.cs
@@ -0,0 +1,357 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MurlocPaladin
+{
+    public partial class MurlocPaladin : Form
+    {
+        public MurlocPaladin()
+        {
+            InitializeComponent();
+        }
+
+        private void btnThatsAwesome_Click(object sender, EventArgs e)
+        {
+            int OldMurk_Eye = 0;
+            int Warleader = 0;
+            int BluegillWarrior = 0;
+            int GrimscaleOracle = 0;
+            int NoFish = 0;
+            int OldMurk_Eye2 = 0;
+            int Warleader2 = 0;
+            int BluegillWarrior2 = 0;
+            int GrimscaleOracle2 = 0;
+            int NoFish2 = 0;
+            int result = 0;
+            int dmgOfOld = 0;
+            int dmgOfWarrior = 0;
+            if (int.TryParse(txtOldMurk_Eye.Text, out OldMurk_Eye) && int.TryParse(txtWarleader.Text, out Warleader) && int.TryParse(txtBluegillWarrior.Text, out BluegillWarrior) && int.TryParse(txtGrimscaleOracle.Text, out GrimscaleOracle) && int.TryParse(txtNoFish.Text, out NoFish)
+                && int.TryParse(txtOldMurk_Eye2.Text, out OldMurk_Eye2) && int.TryParse(txtWarleader2.Text, out Warleader2) && int.TryParse(txtBluegillWarrior2.Text, out BluegillWarrior2) && int.TryParse(txtGrimscaleOracle2.Text, out GrimscaleOracle2) && int.TryParse(txtNoFish2.Text, out NoFish2))
+            {
+                if (OldMurk_Eye >= 0 && Warleader >= 0 && BluegillWarrior >= 0 && GrimscaleOracle >= 0 && NoFish >= 0
+                    && OldMurk_Eye2 >= 0 && Warleader2 >= 0 && BluegillWarrior2 >= 0 && GrimscaleOracle2 >= 0 && NoFish2 >= 0)
+                {
+                    dmgOfWarrior += ((Warleader + Warleader2) * 2 + (GrimscaleOracle + GrimscaleOracle2) * 1 + 2) * BluegillWarrior;
+                    dmgOfOld += ((Warleader + Warleader2) * 2 + (GrimscaleOracle + GrimscaleOracle2) * 1 + (Warleader + Warleader2) + (BluegillWarrior + BluegillWarrior2) + (GrimscaleOracle + GrimscaleOracle2) + (NoFish + NoFish2) + (OldMurk_Eye + OldMurk_Eye2 - 1) + 2) * OldMurk_Eye;
+                    result = dmgOfOld + dmgOfWarrior;
+                    lblResult.Text = result.ToString();
+                }
+                else
+                {
+                    MessageBox.Show("鱼人数目不对啊，你逗我！");
+                }
+            }
+            else
+            {
+                MessageBox.Show("鱼人数目不对啊，你逗我！");
+            }
+
+
+        }
+
+        private void reset()
+        {
+            txtOldMurk_Eye.Text = "0";
+            txtWarleader.Text = "0";
+            txtBluegillWarrior.Text = "0";
+            txtGrimscaleOracle.Text = "0";
+            lblResult.Text = "0";
+
+            txtOldMurk_Eye2.Text = "0";
+            txtWarleader2.Text = "0";
+            txtBluegillWarrior2.Text = "0";
+            txtGrimscaleOracle2.Text = "0";
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            reset();
+        }
+        public void startCaculate()
+        {
+            int OldMurk_Eye = 0;
+            int Warleader = 0;
+            int BluegillWarrior = 0;
+            int GrimscaleOracle = 0;
+            int NoFish = 0;
+            int OldMurk_Eye2 = 0;
+            int Warleader2 = 0;
+            int BluegillWarrior2 = 0;
+            int GrimscaleOracle2 = 0;
+            int NoFish2 = 0;
+            if (int.TryParse(txtOldMurk_Eye.Text, out OldMurk_Eye) && int.TryParse(txtWarleader.Text, out Warleader) && int.TryParse(txtBluegillWarrior.Text, out BluegillWarrior) && int.TryParse(txtGrimscaleOracle.Text, out GrimscaleOracle) && int.TryParse(txtNoFish.Text, out NoFish)
+               && int.TryParse(txtOldMurk_Eye2.Text, out OldMurk_Eye2) && int.TryParse(txtWarleader2.Text, out Warleader2) && int.TryParse(txtBluegillWarrior2.Text, out BluegillWarrior2) && int.TryParse(txtGrimscaleOracle2.Text, out GrimscaleOracle2) && int.TryParse(txtNoFish2.Text, out NoFish2))
+            {
+                List<OldMurk_Eye> oe = new List<OldMurk_Eye>(new OldMurk_Eye[OldMurk_Eye]);
+                List<Warleader> wl = new List<Warleader>(new Warleader[Warleader]);
+                List<BluegillWarrior> bw = new List<BluegillWarrior>(new BluegillWarrior[BluegillWarrior]);
+                List<GrimscaleOracle> go = new List<GrimscaleOracle>(new GrimscaleOracle[GrimscaleOracle]);
+                List<NoFish> nf = new List<NoFish>(new NoFish[NoFish]);
+
+                MurlocList graveYard = new MurlocList();
+                graveYard.OE = oe;
+                graveYard.WL = wl;
+                graveYard.BW = bw;
+                graveYard.GO = go;
+                graveYard.NF = nf;
+            }
+            else
+                MessageBox.Show("鱼人数目不对啊，呜啦啦啦咕噜哇啦！");
+        }
+    }
+
+   
+
+
+
+    //老瞎眼
+    public class OldMurk_Eye : minion
+    {
+        public OldMurk_Eye()
+        {
+            this.attack = 2;
+            this.health = 4;
+            this.isCharge = true;
+        }
+        public OldMurk_Eye(MurlocList oc)
+        {
+            this.attack = 2;
+            this.health = 4;
+            this.isCharge = true;
+            buff(oc);
+        }
+        public override void buff(MurlocList oc)
+        {
+            if (oc.OE.Contains(this))
+                this.attack = 2 + oc.BW.Count() + oc.GO.Count() + oc.NF.Count() + (oc.OE.Count() - 1) + oc.WL.Count();
+            else
+                this.attack = 2 + oc.BW.Count() + oc.GO.Count() + oc.NF.Count() + (oc.OE.Count()) + oc.WL.Count();
+        }
+
+    }
+    //鱼人领军
+    public class Warleader : minion
+    {
+        public Warleader()
+        {
+            this.attack = 3;
+            this.health = 3;
+            this.isCharge = false;
+        }
+        public Warleader(MurlocList oc)
+        {
+            this.attack = 3;
+            this.health = 3;
+            this.isCharge = false;
+            buff(oc);
+        }
+        public override void buff(MurlocList oc)
+        {
+            foreach (minion m in oc.OE)
+            {
+                m.attackUp(2);
+                m.healthUp(1);
+            }
+            foreach (minion m in oc.BW)
+            {
+                m.attackUp(2);
+                m.healthUp(1);
+            }
+            foreach (minion m in oc.GO)
+            {
+                m.attackUp(2);
+                m.healthUp(1);
+            }
+            foreach (minion m in oc.WL)
+            {
+                if (!m.ID.Equals(this.ID))
+                {
+                    m.attackUp(2);
+                    m.healthUp(1);
+                }
+            }
+            foreach (minion m in oc.NF)
+            {
+                m.attackUp(2);
+                m.healthUp(1);
+            }
+        }
+        public override void buffCancel(MurlocList oc)
+        {
+            foreach (minion m in oc.OE)
+            {
+                m.attackDown(2);
+                m.healthDown(1);
+            }
+            foreach (minion m in oc.BW)
+            {
+                m.attackDown(2);
+                m.healthDown(1);
+            }
+            foreach (minion m in oc.GO)
+            {
+                m.attackDown(2);
+                m.healthDown(1);
+            }
+            foreach (minion m in oc.WL)
+            {
+                m.attackDown(2);
+                m.healthDown(1);
+            }
+            foreach (minion m in oc.NF)
+            {
+                m.attackDown(2);
+                m.healthDown(1);
+            }
+        }
+    }
+    //蓝腮战士
+    public class BluegillWarrior : minion
+    {
+        public BluegillWarrior()
+        {
+            this.attack = 2;
+            this.health = 1;
+            this.isCharge = true;
+        }
+        public BluegillWarrior(MurlocList oc)
+        {
+            this.attack = 2;
+            this.health = 1;
+            this.isCharge = true;
+        }
+        public override void buff(MurlocList oc)
+        {
+
+        }
+        public override void buffCancel(MurlocList oc)
+        {
+
+        }
+    }
+    //暗鳞先知
+    public class GrimscaleOracle : minion {
+        public GrimscaleOracle() {
+            this.attack = 1;
+            this.health = 1;
+            this.isCharge = false;
+        }
+        public GrimscaleOracle(MurlocList oc)
+        {
+            this.attack = 1;
+            this.health = 1;
+            this.isCharge = false;
+        }
+        public override void buff(MurlocList oc)
+        {
+            foreach (minion m in oc.OE)
+            {
+                m.attackUp(1);
+            }
+            foreach (minion m in oc.BW)
+            {
+                m.attackUp(1);
+            }
+            foreach (minion m in oc.GO)
+            {
+                if(!m.ID.Equals(this.ID))
+                m.attackUp(1);
+            }
+            foreach (minion m in oc.WL)
+            {
+                m.attackUp(1);
+            }
+            foreach (minion m in oc.NF)
+            {
+                m.attackUp(1);
+            }
+        }
+
+        public override void buffCancel(MurlocList oc)
+        {
+            foreach (minion m in oc.OE)
+            {
+                m.attackDown(1);
+            }
+            foreach (minion m in oc.BW)
+            {
+                m.attackDown(1);
+            }
+            foreach (minion m in oc.GO)
+            {
+                if (!m.ID.Equals(this.ID))
+                    m.attackDown(1);
+            }
+            foreach (minion m in oc.WL)
+            {
+                m.attackDown(1);
+            }
+            foreach (minion m in oc.NF)
+            {
+                m.attackDown(1);
+            }
+        }
+    }
+    //杂牌军
+    public class NoFish : minion { }
+
+    //随从基础数据
+    public class minion
+    {
+        public string ID = Guid.NewGuid().ToString();
+        //攻击
+        public int attack { get; set; }
+        //血量
+        public int health { get; set; }
+        //冲锋
+        public bool isCharge { get; set; }
+        //战吼
+        public virtual void battleCry(MurlocList oc) { }
+        //buff
+        public virtual void buff(MurlocList oc) { }
+        public virtual void buffCancel(MurlocList oc) { }
+        //亡语
+        public virtual void deathRattle(MurlocList oc) { }
+        //攻击力改变
+        public void attackUp(int i)
+        {
+            this.attack += i;
+        }
+        public void attackDown(int i)
+        {
+            if (this.attack <= i)
+                this.attack = 0;
+            else
+                this.attack += -i;
+        }
+        //生命值改变
+        public void healthUp(int i)
+        {
+            this.health += i;
+        }
+        public void healthDown(int i)
+        {
+            if (this.health <= i)
+                this.health = 0;
+            else
+                this.health += -i;
+        }
+    }
+
+    //鱼人列表
+    public class MurlocList
+    {
+        public List<OldMurk_Eye> OE = new List<OldMurk_Eye>();
+        public List<Warleader> WL = new List<Warleader>();
+        public List<BluegillWarrior> BW = new List<BluegillWarrior>();
+        public List<GrimscaleOracle> GO = new List<GrimscaleOracle>();
+        public List<NoFish> NF = new List<NoFish>();
+    }
+}
